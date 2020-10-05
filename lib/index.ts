1eb32f82bd5f9e3d3a85cf2bae60a210a2ac5301
NotVeryVegan <joseph300905@gmail.com>
2020-10-05 14:09:19 +0200

    Fix requests

--- lib/index.ts
+++ lib/index.ts
@@ -1,24 +1,26 @@
-import * as axios from 'axios';
+import * as request from 'request-promise-native';
 
 export class Client {
-    _token: any = null;
+    token: any = null;
 
     /**
      * @method: Initialize client
      * @param {string} token - Bearer access token provided by Discord
      */
     constructor (token: string) {
-        this._token = token;
+        this.token = token;
     }
 
     /**
      * @method: Get user from token
      */
     getUser() {
-        axios.default.get('https://discord.com/api/users/@me', {
-            headers: 'Bearer ' + this._token,
+        request.get('https://discord.com/api/users/@me', {
+            headers: {
+                'Authorization': 'Bearer ' + this.token,
+            },
         })
-            .then(res => { return res.data })
+            .then(res => { return res })
             .catch(err => { throw err });
     }
 
@@ -26,10 +28,12 @@ export class Client {
      * @method: Get guilds of user from token
      */
     getGuilds() {
-        axios.default.get('https://discord.com/api/users/@me/guilds', {
-            headers: 'Bearer ' + this._token,
+        request.get('https://discord.com/api/users/@me/guilds', {
+            headers: {
+                'Authorization': 'Bearer ' + this.token,
+            },
         })
-            .then(res => { return res.data })
-            .catch(err => { throw err })
+            .then(res => { return res })
+            .catch(err => { throw err });
     }
 }
