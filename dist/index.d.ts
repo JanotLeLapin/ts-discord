f774eb1abc7c3b0273043d3835177f8ae8efd9bd
NotVeryVegan <joseph300905@gmail.com>
2020-10-05 11:55:41 +0200

    Build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,9 +1,16 @@
-declare class Client {
-    token: string;
+export declare class Client {
+    _token: any;
+    /**
+     * @method: Initialize client
+     * @param {string} token - Bearer access token provided by Discord
+     */
     constructor(token: string);
     /**
      * @method: Get user from token
      */
     getUser(): void;
+    /**
+     * @method: Get guilds of user from token
+     */
+    getGuilds(): void;
 }
-export default Client;
--- dist/index.js
+++ dist/index.js
@@ -1,21 +1,36 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.Client = void 0;
 var axios = require("axios");
 var Client = /** @class */ (function () {
+    /**
+     * @method: Initialize client
+     * @param {string} token - Bearer access token provided by Discord
+     */
     function Client(token) {
-        this.token = '';
-        this.token = token;
+        this._token = null;
+        this._token = token;
     }
     /**
      * @method: Get user from token
      */
     Client.prototype.getUser = function () {
         axios.default.get('https://discord.com/api/users/@me', {
-            headers: 'Bearer ' + this.token,
+            headers: 'Bearer ' + this._token,
         })
             .then(function (res) { return res.data; })
-            .catch(function (err) { return err.response; });
+            .catch(function (err) { throw err; });
+    };
+    /**
+     * @method: Get guilds of user from token
+     */
+    Client.prototype.getGuilds = function () {
+        axios.default.get('https://discord.com/api/users/@me/guilds', {
+            headers: 'Bearer ' + this._token,
+        })
+            .then(function (res) { return res.data; })
+            .catch(function (err) { throw err; });
     };
     return Client;
 }());
-exports.default = Client;
+exports.Client = Client;
