6e48d70c8c9a0dd4930d252700d48ebf44fb1c86
NotVeryVegan <joseph300905@gmail.com>
2020-10-06 10:52:18 +0200

    Add interfaces

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,27 @@
 import * as axios from 'axios';
 
+interface User {
+    id:            string;
+    username:      string;
+    avatar:        string;
+    discriminator: string;
+    public_flags:  number;
+    flags:         number;
+    locale:        string;
+    mfa_enabled:   boolean;
+}
+
+type feature = "BANNER" | "COMMUNITY" | "ANIMATED_ICON" | "INVITE_SPLASH" | "NEWS";
+interface Guild {
+    id:              string;
+    name:            string;
+    icon:            string;
+    owner:           boolean;
+    permissions:     number;
+    features:        feature[];
+    permissions_new: string;
+}
+
 export class Client {
     token: any = null;
 
@@ -14,26 +36,34 @@ export class Client {
     /**
      * @method: Get user from token
      */
-    getUser() {
-        axios.default.get('https://discord.com/api/users/@me', {
-            headers: {
-                'Authorization': 'Bearer ' + this.token,
-            },
-        })
-            .then(res => { return res })
-            .catch(err => { throw err });
+    getUser(): Promise<User> {
+        return new Promise<User>((resolve, reject) => {
+            axios.default.get('https://discord.com/api/users/@me', {
+                headers: {
+                    'Authorization': 'Bearer ' + this.token,
+                },
+            })
+                .then(res => resolve(res.data))
+                .catch(err => reject(err));
+        });
     }
 
     /**
      * @method: Get guilds of user from token
      */
-    getGuilds() {
-        axios.default.get('https://discord.com/api/users/@me/guilds', {
-            headers: {
-                'Authorization': 'Bearer ' + this.token,
-            },
-        })
-            .then(res => { return res })
-            .catch(err => { throw err });
+    getGuilds(): Promise<Guild[]> {
+        return new Promise<Guild[]>((resolve, reject) => {
+            axios.default.get('https://discord.com/api/users/@me/guilds', {
+                headers: {
+                    'Authorization': 'Bearer ' + this.token,
+                },
+            })
+                .then(res => resolve(res.data))
+                .catch(err => reject(err));
+        });
     }
 }
+
+const client = new Client('nNX8tEtxilhFO6nnrBSRI4bkR5gLBw');
+client.getGuilds()
+    .then(guilds => console.log(guilds[0].features));
