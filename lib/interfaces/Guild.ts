fb0aeb82c0a965892a925ca2ab8f540ec9f078e7
NotVeryVegan <joseph300905@gmail.com>
2020-10-06 15:52:15 +0200

    Fix features type

--- lib/interfaces/Guild.ts
+++ lib/interfaces/Guild.ts
@@ -1,11 +1,25 @@
-type feature = "BANNER" | "COMMUNITY" | "ANIMATED_ICON" | "INVITE_SPLASH" | "NEWS";
+type feature = 
+'INVITE_SPLASH'
+| 'VIP_REGIONS'
+| 'VANITY_URL'
+| 'VERIFIED'
+| 'PARTNERED'
+| 'PUBLIC'
+| 'COMMERCE'
+| 'NEWS'
+| 'DISCOVERABLE'
+| 'FEATURABLE'
+| 'ANIMATED_ICON'
+| 'BANNER'
+| 'PUBLIC_DISABLED'
+| 'WELCOME_SCREEN_ENABLED'
 
 export default interface Guild {
-    id:              string;
-    name:            string;
-    icon:            string;
-    owner:           boolean;
-    permissions:     number;
-    features:        feature[];
+    id: string;
+    name: string;
+    icon: string;
+    owner: boolean;
+    permissions: number;
+    features: feature[];
     permissions_new: string;
 }
