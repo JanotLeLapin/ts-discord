f4fe7f96126435892c63e364ba025a80b745227f
Joseph <joseph300905@gmail.com>
2020-10-06 20:21:12 +0200

    Create connections interface

---
+++ lib/interfaces/Connection.ts
@@ -0,0 +1,23 @@
+type type = 'twitch'
+| 'youtube'
+| 'battle.net'
+| 'steam'
+| 'reddit'
+| 'facebook'
+| 'twitter'
+| 'spotify'
+| 'xbox'
+| 'github';
+
+type visibility = 0 | 1;
+
+export default interface Connection {
+    id: string;
+    name: string;
+    type: type;
+    revoked?: boolean;
+    visibility: visibility;
+    friend_sync: boolean;
+    show_activity: boolean;
+    verified: boolean;
+}
