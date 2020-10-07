3264f84b4e3ac8add6284ae7a792aa87c1b901a9
NotVeryVegan <joseph300905@gmail.com>
2020-10-07 10:15:31 +0200

    Build

---
+++ dist/Client.d.ts
@@ -0,0 +1,23 @@
+import User from './interfaces/User';
+import Guild from './interfaces/Guild';
+import Connection from './interfaces/Connection';
+export default class Client {
+    #private;
+    /**
+     * @method: Initialize client
+     * @param {string} token - OAuth2 bearer access token provided by the OAuth2 API.
+     */
+    constructor(token: string);
+    /**
+     * @method: Get current user
+     */
+    fetchUser(): Promise<User>;
+    /**
+     * @method: Get guilds of current user
+     */
+    fetchGuilds(): Promise<Guild[]>;
+    /**
+     * @method: Get connections of current user
+     */
+    fetchConnections(): Promise<Connection[]>;
+}
--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,23 +1,2 @@
-import User from './interfaces/User';
-import Guild from './interfaces/Guild';
-import Connection from './interfaces/Connection';
-export declare class Client {
-    #private;
-    /**
-     * @method: Initialize client
-     * @param {string} token - OAuth2 bearer access token provided by the OAuth2 API.
-     */
-    constructor(token: string);
-    /**
-     * @method: Get current user
-     */
-    fetchUser(): Promise<User>;
-    /**
-     * @method: Get guilds of current user
-     */
-    fetchGuilds(): Promise<Guild[]>;
-    /**
-     * @method: Get connections of current user
-     */
-    fetchConnections(): Promise<Connection[]>;
-}
+import Client from './Client';
+export { Client, };
